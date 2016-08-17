83ff1ad8fb5b5722a7bc1eb49588871a2ce81cf9
Brett New and Joel Delight <brett+joel@>
2016-08-17 11:32:29 -0700

    add functionality to allow admin to edit posts.

--- app/components/edit-post.js
+++ app/components/edit-post.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  editPost: false,
+  actions: {
+    editPostShow() {
+      this.set('editPost', true);
+    },
+    edit(post) {
+      var params = {
+        title: this.get('title'),
+        author: this.get('author'),
+        category: this.get('category'),
+        timestamp: this.get('timestamp'),
+        content: this.get('content')
+      };
+      this.set('editPost', false);
+      this.sendAction('edit', post, params);
+    }
+  }
 });
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -6,6 +6,16 @@ export default Ember.Route.extend({
   },
   actions: {
 
+    edit(post, params) {
+      Object.keys(params).forEach(function(key){
+        if(params[key]!==undefined) {
+          post.set(key, params[key]);
+        }
+      });
+      post.save();
+      this.transitionTo('admin');
+    },
+
     deletePost(post) {
       post.destroyRecord();
       this.transitionTo('admin');
