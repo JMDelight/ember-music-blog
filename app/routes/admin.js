897068ac63d9cca2066936d09b171cd0d158c978
Brett New and Joel Delight <brett+joel@>
2016-08-18 13:27:48 -0700

    add functionality to allow users to add categories and to view the posts associated with each category.

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -2,7 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('post');
+    return Ember.RSVP.hash({
+      categories: this.store.findAll('category'),
+      posts: this.store.findAll('post')
+  });
   },
   actions: {
 
@@ -27,7 +30,11 @@ export default Ember.Route.extend({
     },
     save3(params) {
       var newPost = this.store.createRecord('post', params);
-      newPost.save();
+      var category = params.category;
+      category.get('posts').addObject(newPost);
+      newPost.save().then(function() {
+        return category.save();
+      });
       this.transitionTo('admin');
     }
   }
